a312fb0816aa0b25d7e7be92945413e4c0221af6
Petras Vestartas <petrasvestartas@gmail.com>
2022-03-02 19:35:59 +0100

    Development of Timber Plate Joint Samples

--- net/joinery_solver_gh/Properties/Resources.Designer.cs
+++ net/joinery_solver_gh/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace joinery_solver_gh.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap case_2_cross {
+            get {
+                object obj = ResourceManager.GetObject("case_2_cross", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap compas_wood {
             get {
                 object obj = ResourceManager.GetObject("compas_wood", resourceCulture);
