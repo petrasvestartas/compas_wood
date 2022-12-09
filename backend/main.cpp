3a5d2c4c2d35946628b99ad4ca6c84f0905042b3
Petras Vestartas <petrasvestartas@gmail.com>
2022-12-09 08:44:52 +0100

    ADD bounding rectangle implementation in cgal_rectangle_util.h

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,9 +1,9 @@
 #include "stdafx.h"
 
 // temp
-//#include "wood_main.h"
-//#include "wood_xml.h"	 // xml parser and viewer customization
-//#include "viewer_wood.h" // viewer
+// #include "wood_main.h"
+// #include "wood_xml.h"	 // xml parser and viewer customization
+// #include "viewer_wood.h" // viewer
 #include "wood_test.h" // test
 
 #include <gtest/gtest.h>
@@ -17,53 +17,73 @@ TEST(HelloTest, BasicAssertions)
 	EXPECT_EQ(7 * 6, 42);
 }
 
-//#include <polylabel/polylabel.h>
+// #include <polylabel/polylabel.h>
 #include <polylabel/cgal_polylabel.h>
+#include "src/wood/include/cgal_rectangle_util.h"
 int main(int argc, char **argv)
 {
 
-	std::vector<CGAL_Polyline> polylines;
+	// IK::Segment_3 segment(IK::Point_3(0, 0, 0), IK::Point_3(1, 1, 0));
+	// // std::cout << cgal_rectangle_util::angle_to_xaxis(segment) << "\n";
 
-	CGAL_Polyline p0{
-		IK::Point_3(-51.268164698738, 124.403259575835, 0),
-		IK::Point_3(-49.9053069162896, 125.207270883064, 0),
-		IK::Point_3(-49.2387999055025, 123.598641360433, 0),
-		IK::Point_3(-51.268164698738, 124.403259575835, 0),
-	};
+	// CGAL_Polyline polygon{
+	// 	IK::Point_3(11.2247596176396, -4.60190643733344, 48.9387618537435),
+	// 	IK::Point_3(-5.73799008527948, 27.7396887390699, 38.9994193935361),
+	// 	IK::Point_3(-7.18751465466158, 52.2109764075134, 74.5663582911656),
+	// 	IK::Point_3(1.68332949862521, 18.8956298287167, 52.2485552589767),
+	// 	IK::Point_3(14.0704361211272, 1.10346344393741, 69.2793807577245),
+	// 	IK::Point_3(18.9418950031543, 14.2294672539151, 109.735250000281),
+	// 	IK::Point_3(33.4352381327694, -30.7000564810127, 89.2118909021016),
+	// 	IK::Point_3(11.2247596176396, -4.60190643733344, 48.9387618537435),
+	// };
 
-	CGAL_Polyline p1{
-		IK::Point_3(-80.6608706869908, 113.550451407233, 0),
-		IK::Point_3(-105.295437496349, 124.537127782527, 0),
-		IK::Point_3(-76.4480884206953, 152.30682138802, 0),
-		IK::Point_3(-23.6934094178782, 139.390066049447, 0),
-		IK::Point_3(-40.1282334662578, 100.282298806669, 0),
-		IK::Point_3(-69.6350088663798, 87.7972305312764, 0),
-		IK::Point_3(-74.8538706538141, 108.464053813429, 0),
-		IK::Point_3(-80.6608706869908, 113.550451407233, 0),
-	};
+	// CGAL_Polyline rectangle;
+	// cgal_rectangle_util::bounding_rectangle(polygon, rectangle);
+	// for (auto &point : rectangle)
+	// 	std::cout << point.hx() << " " << point.hy() << " " << point.hz() << "\n";
 
-	CGAL_Polyline p2{
-		IK::Point_3(-73.4808769486925, 119.997690099402, 0),
-		IK::Point_3(-90.6424155869293, 123.188472670033, 0),
-		IK::Point_3(-77.6978215210285, 136.742511096638, 0),
-		IK::Point_3(-73.4808769486925, 119.997690099402, 0),
-	};
+	// std::vector<CGAL_Polyline>
+	// 	polylines;
 
-	polylines.push_back(p0);
-	polylines.push_back(p1);
-	polylines.push_back(p2);
+	// CGAL_Polyline p0{
+	// 	IK::Point_3(-51.268164698738, 124.403259575835, 0),
+	// 	IK::Point_3(-49.9053069162896, 125.207270883064, 0),
+	// 	IK::Point_3(-49.2387999055025, 123.598641360433, 0),
+	// 	IK::Point_3(-51.268164698738, 124.403259575835, 0),
+	// };
 
-	std::tuple<IK::Point_3, IK::Plane_3, double> result = get_polylabel(polylines, 1.0);
-	std::cout << std::get<0>(result).hx() << " " << std::get<0>(result).hy() << " " << std::get<0>(result).hz() << std::endl;
-	std::cout << std::get<1>(result).orthogonal_vector().hx() << " " << std::get<1>(result).orthogonal_vector().hy() << " " << std::get<1>(result).orthogonal_vector().hz() << std::endl;
-	std::cout << std::get<2>(result) << std::endl;
+	// CGAL_Polyline p1{
+	// 	IK::Point_3(-80.6608706869908, 113.550451407233, 0),
+	// 	IK::Point_3(-105.295437496349, 124.537127782527, 0),
+	// 	IK::Point_3(-76.4480884206953, 152.30682138802, 0),
+	// 	IK::Point_3(-23.6934094178782, 139.390066049447, 0),
+	// 	IK::Point_3(-40.1282334662578, 100.282298806669, 0),
+	// 	IK::Point_3(-69.6350088663798, 87.7972305312764, 0),
+	// 	IK::Point_3(-74.8538706538141, 108.464053813429, 0),
+	// 	IK::Point_3(-80.6608706869908, 113.550451407233, 0),
+	// };
+
+	// CGAL_Polyline p2{
+	// 	IK::Point_3(-73.4808769486925, 119.997690099402, 0),
+	// 	IK::Point_3(-90.6424155869293, 123.188472670033, 0),
+	// 	IK::Point_3(-77.6978215210285, 136.742511096638, 0),
+	// 	IK::Point_3(-73.4808769486925, 119.997690099402, 0),
+	// };
+
+	// polylines.push_back(p0);
+	// polylines.push_back(p1);
+	// polylines.push_back(p2);
+
+	// std::tuple<IK::Point_3, IK::Plane_3, double> result = cgal_polylabel::get_polylabel(polylines, 1.0);
+	// std::cout << std::get<0>(result).hx() << " " << std::get<0>(result).hy() << " " << std::get<0>(result).hz() << std::endl;
+	// std::cout << std::get<1>(result).orthogonal_vector().hx() << " " << std::get<1>(result).orthogonal_vector().hy() << " " << std::get<1>(result).orthogonal_vector().hz() << std::endl;
+	// std::cout << std::get<2>(result) << std::endl;
 	// 	mapbox::geometry::polygon<double>
 	// 		polygon = {
 	// 			{{25.003161, 16.395586}, {0.368594, 27.382262}, {29.215943, 55.151956}, {81.970622, 42.2352}, {65.535798, 3.127433}, {36.029023, -9.357635}, {30.810161, 11.309188}},
 	// 			{{32.183155, 22.842825}, {15.021616, 26.033607}, {27.96621, 39.587646}}}; // Get polygon data from somewhere.
 	// std::array<double, 3> center_and_radius = mapbox::polylabel(polygon, 1.0);
 	// std::cout << center_and_radius[0] << "," << center_and_radius[1] << "," << center_and_radius[2] << std::endl;
-	return 0;
 
 	::testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
---
+++ backend/src/wood/include/cgal_rectangle_util.h
@@ -0,0 +1,377 @@
+#pragma once
+
+namespace cgal_rectangle_util
+{
+
+    namespace internal
+    {
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * it checks if points are clockwise or counter clockwise
+         * The z-value of the cross product of segments | (a, b) and (a, c). Positive means c is ccw | from (a, b), negative cw. Zero means its collinear.
+         *
+         * @param a point
+         * @param b point
+         * @param c point
+         * @return float value
+         */
+
+        inline float ccw(const IK::Point_3 &a, const IK::Point_3 &b, const IK::Point_3 &c)
+        {
+            return (b.x() - a.x()) * (c.y() - a.y()) - (b.y() - a.y()) * (c.x() - a.x());
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * Returns true if a is lexicographically before b.
+         *
+         * @param a point
+         * @param b point
+         * @return bool value
+         */
+        inline bool is_left_of(const IK::Point_3 &a, const IK::Point_3 &b)
+        {
+            return (a.x() < b.x() || (a.x() == b.x() && a.y() < b.y()));
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * struct for sorting points
+         */
+        struct ccw_sorter
+        {
+            const IK::Point_3 &pivot;
+
+            ccw_sorter(const IK::Point_3 &in_pivot) : pivot(in_pivot) {}
+
+            bool operator()(const IK::Point_3 &a, const IK::Point_3 &b)
+            {
+                return ccw(pivot, a, b) < 0;
+            }
+        };
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * the length of segment (a, b).
+         * @param a point
+         * @param b point
+         * @return float value that represent the length of a line
+         */
+        inline float len(const IK::Point_3 &a, const IK::Point_3 &b)
+        {
+            return std::sqrt((b.x() - a.x()) * (b.x() - a.x()) + (b.y() - a.y()) * (b.y() - a.y()));
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * The unsigned distance of p from segment (a, b).
+         * @param a point
+         * @param b point
+         * @return float value represented the distance
+         */
+        inline float dist(const IK::Point_3 &a, const IK::Point_3 &b, const IK::Point_3 &p)
+        {
+            return std::fabs((b.x() - a.x()) * (a.y() - p.y()) - (b.y() - a.y()) * (a.x() - p.x())) / len(a, b);
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * Returns the index of the farthest point from segment (a, b).
+         * @param a point
+         * @param b point
+         * @param v points to search from
+         * @return size_t index of the farthest point
+         */
+        inline size_t get_farthest(const IK::Point_3 &a, const IK::Point_3 &b, const std::vector<IK::Point_3> &v)
+        {
+            size_t idxMax = 0;
+            float distMax = dist(a, b, v[idxMax]);
+
+            for (size_t i = 1; i < v.size(); ++i)
+            {
+                float distCurr = dist(a, b, v[i]);
+                if (distCurr > distMax)
+                {
+                    idxMax = i;
+                    distMax = distCurr;
+                }
+            }
+
+            return idxMax;
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by quick_hull algorithm
+         * Recursive call of the quick_hull algorithm.
+         * This function is used by the main function below
+         * @param [in] v point
+         * @param [in] a point
+         * @param [in] b point
+         * @param [out] hull points
+         */
+        inline void quick_hull(const std::vector<IK::Point_3> &v, const IK::Point_3 &a, const IK::Point_3 &b, std::vector<IK::Point_3> &hull)
+        {
+            if (v.empty())
+                return;
+
+            IK::Point_3 f = v[get_farthest(a, b, v)];
+
+            // Collect points to the left of segment (a, f)
+            std::vector<IK::Point_3> left;
+            for (auto p : v)
+            {
+                if (ccw(a, f, p) > 0)
+                {
+                    left.emplace_back(p);
+                }
+            }
+            quick_hull(left, a, f, hull);
+
+            // Add f to the hull
+            hull.emplace_back(f);
+
+            // Collect points to the left of segment (f, b)
+            std::vector<IK::Point_3> right;
+            for (auto p : v)
+            {
+                if (ccw(f, b, p) > 0)
+                {
+                    right.emplace_back(p);
+                }
+            }
+            quick_hull(right, f, b, hull);
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by bounding_rectangle algorithm
+         * get angle of an edge to its projects to the x-axis, this is the helper fuction of bounding_rectangle
+         *
+         * @param edge polyline segment
+         * @return angle
+         */
+        inline double angle_to_xaxis(IK::Segment_3 &edge)
+        {
+
+            auto delta = edge[0] - edge[1];
+            return -std::atan(delta.hy() / delta.hx());
+        }
+
+        /**
+         * WARNING user must not use this method - this method is used by bounding_rectangle algorithm
+         * get edge after rotation to x axis, this is the helper fuction of bounding_rectangle
+         *
+         * @param vector segment direction
+         * @param angle angle in radians
+         * @return rotated vector
+         */
+        inline IK::Point_3 rotate_to_xaxis(IK::Point_3 &point, const double &angle)
+        {
+            return IK::Point_3(point.hx() * std::cos(angle) - point.hy() * std::sin(angle), point.hx() * std::sin(angle) + point.hy() * std::cos(angle), 0);
+        }
+    }
+
+    /**
+     * get transformation from 3D to 2D and invcerse from a polyline
+     * the 3D plane is computed from the start point of the polyline
+     * and an the sum of the consecutive edges cross products
+     *
+     * @param [in] polyline input CGAL polyline
+     * @param [out] xform_to_xy transformation from 3D to XY
+     * @param [out] xform_to_xy_inv transformation from XY to 3D
+     * @return returns true if the polyline has more than 3 points, else not valid
+     */
+    inline bool orient_polyline_to_xy_and_back(CGAL_Polyline &polyline, CGAL::Aff_transformation_3<IK> &xform_to_xy, CGAL::Aff_transformation_3<IK> &xform_to_xy_inv)
+    {
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // Orient from 3D to 2D
+        /////////////////////////////////////////////////////////////////////////////////////
+        if (polyline.size() < 3)
+            return false;
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // Get center polyline plane
+        /////////////////////////////////////////////////////////////////////////////////////
+        IK::Point_3 center;
+        IK::Plane_3 plane;
+        cgal_polyline_util::get_fast_plane(polyline, center, plane);
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        // Create Transformation and assign outputs
+        /////////////////////////////////////////////////////////////////////////////////////
+        xform_to_xy = cgal_xform_util::PlaneToXY(polyline[0], plane);
+        xform_to_xy_inv = xform_to_xy.inverse();
+        return true;
+    }
+
+    /**
+     * quick_hull algorithm
+     * https://en.wikipedia.org/wiki/quick_hull
+     * https://github.com/MiguelVieira/ConvexHull2D/blob/master/ConvexHull.cpp
+     *
+     * @param [in, out] v input polylines or a list of points
+     * @return points that re
+     */
+    inline CGAL_Polyline quick_hull(const CGAL_Polyline &v)
+    {
+        std::vector<IK::Point_3> hull;
+
+        // Start with the leftmost and rightmost points.
+        IK::Point_3 a = *min_element(v.begin(), v.end(), internal::is_left_of);
+        IK::Point_3 b = *max_element(v.begin(), v.end(), internal::is_left_of);
+
+        // Split the points on either side of segment (a, b)
+        std::vector<IK::Point_3> left, right;
+        for (auto p : v)
+        {
+            internal::ccw(a, b, p) > 0 ? left.emplace_back(p) : right.emplace_back(p);
+        }
+
+        // Be careful to add points to the hull
+        // in the correct order. Add our leftmost point.
+        hull.emplace_back(a);
+
+        // Add hull points from the left (top)
+        internal::quick_hull(left, a, b, hull);
+
+        // Add our rightmost point
+        hull.emplace_back(b);
+
+        // Add hull points from the right (bottom)
+        internal::quick_hull(right, b, a, hull);
+
+        return hull;
+    }
+
+    /**
+     * get bounding rectangle of a polygon
+     * the polygon can be rotated in 3d, because the function orients the polygon to XY plan
+     * port from the C# version: https://github.com/cansik/LongLiveTheSquare/blob/master/U4LongLiveTheSquare/MinimalBoundingBox.cs
+     *
+     * EXAMPLE:
+     * 	CGAL_Polyline polygon{
+     *        IK::Point_3(0.766806731645467, -18.3800901708968, 0),
+     *        IK::Point_3(-31.4979699097944, 1.40597494719874, 0),
+     *        IK::Point_3(-20.255887456331, 43.1141008495478, 0),
+     *        IK::Point_3(-13.9603212823915, 2.53018319254508, 0),
+     *        IK::Point_3(13.3579390795244, -1.17970401709784, 0),
+     *        IK::Point_3(37.0787330563321, 34.457697360381, 0),
+     *        IK::Point_3(53.4921734383886, -14.33294048765, 0),
+     *        IK::Point_3(0.766806731645467, -18.3800901708968, 0),
+     *    };
+     *
+     *    CGAL_Polyline rectangle;
+     *    cgal_rectangle_util::bounding_rectangle(polygon, rectangle);
+     *    for (auto &point : rectangle)
+     *        std::cout << point.hx() << " " << point.hy() << " " << point.hz() << "\n";
+     *
+     *
+     * @param [in] polygon input polyline
+     * @param [out] result output rectangle
+     * @return bool flag if the result is valid
+     */
+    inline bool bounding_rectangle(CGAL_Polyline &polygon, CGAL_Polyline &result)
+    {
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Orient polygon to 2D
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        CGAL::Aff_transformation_3<IK> xform_to_xy, xform_to_xy_inv;
+        orient_polyline_to_xy_and_back(polygon, xform_to_xy, xform_to_xy_inv);
+        CGAL_Polyline polygon_copy = polygon;
+        cgal_polyline_util::Transform(polygon_copy, xform_to_xy);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // calculate the convex hull
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        auto hull_points = quick_hull(polygon_copy);
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // check if no bounding box available
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        if (hull_points.size() <= 2)
+            return false;
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // foreach edge of the convex hull
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        IK::Point_2 p0(0, 0);
+        IK::Point_2 p1(0, 0);
+        CGAL::Iso_rectangle_2<IK> rectangle(p0, p1);
+        double min_angle = 0;
+
+        for (int i = 0; i < hull_points.size(); i++)
+        {
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // get polyline index
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            auto next_index = i + 1;
+            auto current = hull_points[i];
+            auto next = hull_points[next_index % hull_points.size()];
+            auto segment = IK::Segment_3(current, next);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // min / max points
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            double top = -std::numeric_limits<double>::max();
+            double bottom = std::numeric_limits<double>::max();
+            double left = std::numeric_limits<double>::max();
+            double right = -std::numeric_limits<double>::max();
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // get angle of segment to x axis
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            auto angle = internal::angle_to_xaxis(segment);
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // rotate every point and get min and max values for each direction
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            for (auto &p : hull_points)
+            {
+                auto rotated_point = internal::rotate_to_xaxis(p, angle);
+
+                top = std::max(top, rotated_point.hy());
+                bottom = std::min(bottom, rotated_point.hy());
+                left = std::min(left, rotated_point.hx());
+                right = std::max(right, rotated_point.hx());
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // create a rectangle with the min and max values
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            auto rectangle_temp = CGAL::Iso_rectangle_2<IK>(IK::Point_2(left, bottom), IK::Point_2(right, top));
+
+            // check if this is the first rectangle or if the current one is smaller
+            if (rectangle.is_degenerate() || rectangle_temp.area() < rectangle.area())
+            {
+                rectangle = rectangle_temp;
+                min_angle = angle;
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Convert to CGAL polyline and rotate to axis
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        result = {
+            IK::Point_3(rectangle.min().hx(), rectangle.min().hy(), 0),
+            IK::Point_3(rectangle.min().hx(), rectangle.max().hy(), 0),
+            IK::Point_3(rectangle.max().hx(), rectangle.max().hy(), 0),
+            IK::Point_3(rectangle.max().hx(), rectangle.min().hy(), 0),
+        };
+
+        // rotate axis algined box back
+        for (auto &p : result)
+        {
+            p = internal::rotate_to_xaxis(p, -min_angle);
+            p = p.transform(xform_to_xy_inv);
+        }
+        result.emplace_back(result.front());
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Orient back to 3D
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        return true;
+    }
+
+}
