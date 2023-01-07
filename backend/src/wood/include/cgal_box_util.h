ddea16e1c01562717a10aae7c0bfccf3b6a5977f
Petras Vestartas <petrasvestartas@gmail.com>
2023-01-07 19:49:28 +0100

    MISC finish splitting header and cpp of cgal_box_util

--- backend/src/wood/include/cgal_box_util.h
+++ backend/src/wood/include/cgal_box_util.h
@@ -1,106 +1,113 @@
-#pragma once
-// #include "../../stdafx.h"
-// #include "cgal_vector_util.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DEVELOPER:
+// Petras Vestartas, petasvestartas@gmail.com
+// Funding: NCCR Digital Fabrication and EPFL
+//
+// HISTORY:
+// 1) The first version was written during the PhD 8928 thesis of Petras Vestartas called:
+// Design-to-Fabrication Workflow for Raw-Sawn-Timber using Joinery Solver, 2017-2021
+// 2) The translation from C# to C++ was started during the funding of NCCR in two steps
+// A - standalone C++ version of the joinery solver and B - integration to COMPAS framework (Python Pybind11)
+//
+// RESTRICTIONS:
+// The code cannot be used for commercial reasons
+// If you would like to use or change the code for research or educational reasons,
+// please contact the developer first
+//
+// 3RD PARTY LIBRARIES:
+// Separating axis theorem:
+// https://stackoverflow.com/questions/47866571/simple-oriented-bounding-box-obb-collision-detection-explaining
+// https://github.com/alxschwrz/Separating-Axis-Theorem
+// https://github.com/inzombiak/SATTest
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef CGAL_BOX_UTIL_H
+#define CGAL_BOX_UTIL_H
 
 namespace cgal_box_util
 {
 
     namespace internal
     {
-        inline IK::Vector_3 vector_at(const IK::Vector_3 (&plane)[4], const double s, const double &&t, const double &c)
-        {
-            return (plane[0] + s * plane[1] + t * plane[2] + c * plane[3]);
-        }
+        /**
+         * Get a point on a plane,
+         *
+         * @param [in] plane Plane made as a C-style array of length 4: origin and x,y,z vectors
+         * @param [in] x_axis The first coordinate of the vector
+         * @param [in] y_axis The second coordinate of the vector
+         * @param [in] z_axis The third coordinate of the vector
+         * @return Vector that is a point on a plane
+         */
+        IK::Vector_3 vector_at(const IK::Vector_3 (&plane)[4], const double &x_axis, const double &y_axis, const double &z_axis);
 
-        inline IK::Point_3 point_at(const IK::Vector_3 (&plane)[5], const double &s, const double &t, const double &c)
-        {
-            return IK::Point_3(
-                plane[0].x() + s * plane[1].x() + t * plane[2].x() + c * plane[3].x(),
-                plane[0].y() + s * plane[1].y() + t * plane[2].y() + c * plane[3].y(),
-                plane[0].z() + s * plane[1].z() + t * plane[2].z() + c * plane[3].z()
+        /**
+         * Get a point on a box
+         * The same as "Get a point on a plane"
+         *
+         * @param [in] box Plane made as a C-style array of length 5: origin and x,y,z vectors, and one vector for box size
+         * @param [in] x_axis The first coordinate of the vector
+         * @param [in] y_axis The second coordinate of the vector
+         * @param [in] z_axis The third coordinate of the vector
+         * @return Vector that is a point on a plane
+         */
+        IK::Point_3 point_at(const IK::Vector_3 (&box)[5], const double &x_axis, const double &y_axis, const double &z_axis);
 
-            );
-        }
+        /**
+         * Get box corners
+         *
+         * @param [in] box Plane made as a C-style array of length 5: origin and x,y,z vectors, and one vector for box size, each vector is the length of the box edge lengths
+         * @param [out] corners Eight box corners
+         */
+        void get_corners(const IK::Vector_3 (&box)[5], CGAL_Polyline &corners);
 
-        inline void get_corners(const IK::Vector_3 (&box)[5], CGAL_Polyline &corners)
-        {
-            corners = CGAL_Polyline(8);
+        /**
+         * Get two rectangles of the box
+         * Each rectangle has 5 points, total 10 points as closed polylines
+         *
+         * @param [in] box Plane made as a C-style array of length 5: origin and x,y,z vectors, and one vector for box size, each vector is the length of the box edge lengths
+         * @param [out] corners Ten box corners
+         */
+        void get_two_box_rectangles(const IK::Vector_3 (&box)[5], CGAL_Polyline &corners);
 
-            corners[0] = point_at(box, box[4].x(), box[4].y(), -box[4].z());
-            corners[1] = point_at(box, -box[4].x(), box[4].y(), -box[4].z());
-            corners[3] = point_at(box, box[4].x(), -box[4].y(), -box[4].z());
-            corners[2] = point_at(box, -box[4].x(), -box[4].y(), -box[4].z());
+        /**
+         * Get corners of the CGAL box
+         *
+         * @param [in] box CGAL box
+         * @param [out] corners Eight box corners
+         */
+        void get_corners_box(const CGAL::Bbox_3& box, CGAL_Polyline &corners);
 
-            corners[4] = point_at(box, box[4].x(), box[4].y(), box[4].z());
-            corners[5] = point_at(box, -box[4].x(), box[4].y(), box[4].z());
-            corners[7] = point_at(box, box[4].x(), -box[4].y(), box[4].z());
-            corners[6] = point_at(box, -box[4].x(), -box[4].y(), box[4].z());
-        }
+        /**
+         * Get two rectangles of the CGAL box
+         *
+         * @param [in] box CGAL box
+         * @param [out] corners Ten box corners
+         */
+        void get_two_box_rectangles(const CGAL::Bbox_3& box, CGAL_Polyline &corners);
 
-        inline void get_corners_view(const IK::Vector_3 (&box)[5], CGAL_Polyline &corners)
-        {
-            corners = CGAL_Polyline(10);
-
-            corners[0] = point_at(box, box[4].x(), box[4].y(), -box[4].z());
-            corners[1] = point_at(box, -box[4].x(), box[4].y(), -box[4].z());
-            corners[2] = point_at(box, -box[4].x(), -box[4].y(), -box[4].z());
-            corners[3] = point_at(box, box[4].x(), -box[4].y(), -box[4].z());
-            corners[4] = point_at(box, box[4].x(), box[4].y(), -box[4].z());
-
-            corners[5] = point_at(box, box[4].x(), box[4].y(), box[4].z());
-            corners[6] = point_at(box, -box[4].x(), box[4].y(), box[4].z());
-            corners[7] = point_at(box, -box[4].x(), -box[4].y(), box[4].z());
-            corners[8] = point_at(box, box[4].x(), -box[4].y(), box[4].z());
-            corners[9] = point_at(box, box[4].x(), box[4].y(), box[4].z());
-        }
-
-        inline void get_corners_box(const CGAL::Bbox_3 box, CGAL_Polyline &corners)
-        {
-            corners = CGAL_Polyline(8);
-
-            corners[0] = IK::Point_3(box.xmin(), box.ymin(), box.zmin());
-            corners[1] = IK::Point_3(box.xmin(), box.ymin(), box.zmax());
-            corners[2] = IK::Point_3(box.xmin(), box.ymax(), box.zmax());
-            corners[3] = IK::Point_3(box.xmin(), box.ymax(), box.zmin());
-
-            corners[4] = IK::Point_3(box.xmax(), box.ymin(), box.zmin());
-            corners[5] = IK::Point_3(box.xmax(), box.ymin(), box.zmax());
-            corners[6] = IK::Point_3(box.xmax(), box.ymax(), box.zmax());
-            corners[7] = IK::Point_3(box.xmax(), box.ymax(), box.zmin());
-        }
-
-        inline void get_corners_box_view(const CGAL::Bbox_3 box, CGAL_Polyline &corners)
-        {
-            corners = CGAL_Polyline(10);
-
-            corners[0] = IK::Point_3(box.xmin(), box.ymin(), box.zmin());
-            corners[1] = IK::Point_3(box.xmin(), box.ymin(), box.zmax());
-            corners[2] = IK::Point_3(box.xmin(), box.ymax(), box.zmax());
-            corners[3] = IK::Point_3(box.xmin(), box.ymax(), box.zmin());
-            corners[4] = IK::Point_3(box.xmin(), box.ymin(), box.zmin());
-
-            corners[5] = IK::Point_3(box.xmax(), box.ymin(), box.zmin());
-            corners[6] = IK::Point_3(box.xmax(), box.ymin(), box.zmax());
-            corners[7] = IK::Point_3(box.xmax(), box.ymax(), box.zmax());
-            corners[8] = IK::Point_3(box.xmax(), box.ymax(), box.zmin());
-            corners[9] = IK::Point_3(box.xmax(), box.ymin(), box.zmin());
-        }
-
-        inline bool get_separating_plane(const IK::Vector_3 &relative_position, const IK::Vector_3 &plane, const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5])
-        {
-            return (fabs(relative_position * plane) >
-                    (fabs((box1[1] * box1[4].x()) * plane) +
-                     fabs((box1[2] * box1[4].y()) * plane) +
-                     fabs((box1[3] * box1[4].z()) * plane) +
-                     fabs((box2[1] * box2[4].x()) * plane) +
-                     fabs((box2[2] * box2[4].y()) * plane) +
-                     fabs((box2[3] * box2[4].z()) * plane)));
-        }
+        /**
+         * Check if there's a separating plane in between the selected axes
+         * https://stackoverflow.com/questions/47866571/simple-oriented-bounding-box-obb-collision-detection-explaining
+         * https://github.com/alxschwrz/Separating-Axis-Theorem
+         * https://github.com/inzombiak/SATTest
+         *
+         * @param [in] relative_position difference between box origins
+         * @param [in] axis The axis to check against
+         * @param [in] box1 First box as a 5 vector C array
+         * @param [in] box2 Second box as a 5 vector C array
+         * @return return true If we find an axis that separates the both polygons, then the polygons cannot overlap! This works only for convex polygons!
+         */
+        bool get_separating_plane(const IK::Vector_3 &relative_position, const IK::Vector_3 &axis, const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5]);
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // Native Box Collision
         ///////////////////////////////////////////////////////////////////////////////////////////////
-        // define the operations to be used in our 3D vertices
+
+        /**
+         * Minimal vector implementation with operators
+         *
+         */
         struct vec3
         {
             double x, y, z;
@@ -110,85 +117,51 @@ namespace cgal_box_util
             vec3 operator*(const double &rhs) const { return vec3{x * rhs, y * rhs, z * rhs}; }
         };
 
-        // set the relevant elements of our oriented bounding box
+        /**
+         * Oriented bounding box
+         */
         struct obb
         {
             vec3 Pos, AxisX, AxisY, AxisZ, Half_size;
         };
 
-        // check if there's a separating plane in between the selected axes
-        inline bool get_separating_plane(const vec3 &relative_position, const vec3 &plane, const obb &box1, const obb &box2)
+        /**
+         * Check if there's a separating plane in between the selected axes
+         * https://stackoverflow.com/questions/47866571/simple-oriented-bounding-box-obb-collision-detection-explaining
+         * https://github.com/alxschwrz/Separating-Axis-Theorem
+         * https://github.com/inzombiak/SATTest
+         *
+         * @param [in] relative_position difference between box origins
+         * @param [in] axis The axis to check against
+         * @param [in] box1 First box as a 5 vector custom struct "vec3"
+         * @param [in] box2 Second box as a 5 vector custom struct "vec3"
+         * @return return true If we find an axis that separates the both polygons, then the polygons cannot overlap! This works only for convex polygons!
+         */
+        bool get_separating_plane(const vec3 &relative_position, const vec3 &axis, const obb &box1, const obb &box2);
 
-        {
-            // CGAL_Debug(relative_position);
+        /**
+         * Test for separating planes in all 15 axes
+         * https://stackoverflow.com/questions/47866571/simple-oriented-bounding-box-obb-collision-detection-explaining
+         * https://github.com/alxschwrz/Separating-Axis-Theorem
+         * https://github.com/inzombiak/SATTest
+         *
+         * @param [in] box1 First box as a 5 vector C array
+         * @param [in] box2 Second box as a 5 vector C array
+         * @return return true if there is a collision in at least one of the projections
+         */
+        bool get_collision(const obb &box1, const obb &box2);
 
-            return (fabs(relative_position * plane) >
-                    (fabs((box1.AxisX * box1.Half_size.x) * plane) +
-                     fabs((box1.AxisY * box1.Half_size.y) * plane) +
-                     fabs((box1.AxisZ * box1.Half_size.z) * plane) +
-                     fabs((box2.AxisX * box2.Half_size.x) * plane) +
-                     fabs((box2.AxisY * box2.Half_size.y) * plane) +
-                     fabs((box2.AxisZ * box2.Half_size.z) * plane)));
-        }
-
-        // test for separating planes in all 15 axes
-        inline bool get_collision(const obb &box1, const obb &box2)
-        {
-            static vec3 relative_position;
-            relative_position = box2.Pos - box1.Pos;
-
-            return !(get_separating_plane(relative_position, box1.AxisX, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisY, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisZ, box1, box2) ||
-                     get_separating_plane(relative_position, box2.AxisX, box1, box2) ||
-                     get_separating_plane(relative_position, box2.AxisY, box1, box2) ||
-                     get_separating_plane(relative_position, box2.AxisZ, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisX ^ box2.AxisX, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisX ^ box2.AxisY, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisX ^ box2.AxisZ, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisY ^ box2.AxisX, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisY ^ box2.AxisY, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisY ^ box2.AxisZ, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisZ ^ box2.AxisX, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisZ ^ box2.AxisY, box1, box2) ||
-                     get_separating_plane(relative_position, box1.AxisZ ^ box2.AxisZ, box1, box2));
-        }
-
-        inline bool get_collision_native(const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5])
-        {
-            // Test
-            //  create two obbs
-            obb A, B;
-
-            // set the first obb's properties
-            A.Pos = {box1[0].x(), box1[0].y(), box1[0].z()}; // set its center position
-
-            // set the half size
-            A.Half_size.x = box1[4].x();
-            A.Half_size.y = box1[4].y();
-            A.Half_size.z = box1[4].z();
-
-            // set the axes orientation
-            A.AxisX = {box1[1].x(), box1[1].y(), box1[1].z()};
-            A.AxisY = {box1[2].x(), box1[2].y(), box1[2].z()};
-            A.AxisZ = {box1[3].x(), box1[3].y(), box1[3].z()};
-
-            // set the second obb's properties
-            B.Pos = {box2[0].x(), box2[0].y(), box2[0].z()}; // set its center position
-
-            // set the half size
-            B.Half_size.x = box2[4].x();
-            B.Half_size.y = box2[4].y();
-            B.Half_size.z = box2[4].z();
-
-            // set the axes orientation
-            B.AxisX = {box2[1].x(), box2[1].y(), box2[1].z()};
-            B.AxisY = {box2[2].x(), box2[2].y(), box2[2].z()};
-            B.AxisZ = {box2[3].x(), box2[3].y(), box2[3].z()};
-
-            // run the code and get the result as a message
-            return get_collision(A, B);
-        }
+        /**
+         * Test for separating planes in all 15 axes
+         * https://stackoverflow.com/questions/47866571/simple-oriented-bounding-box-obb-collision-detection-explaining
+         * https://github.com/alxschwrz/Separating-Axis-Theorem
+         * https://github.com/inzombiak/SATTest
+         *
+         * @param [in] box1 First box as a 5 vector C array
+         * @param [in] box2 Second box as a 5 vector C array
+         * @return return true if there is a collision in at least one of the projections
+         */
+        bool get_collision_native(const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5]);
     }
 
     /**
@@ -197,16 +170,7 @@ namespace cgal_box_util
      * @param plane c array that is composed of 4 IK::Vector_3
      * @param xform CGAL 4_4 transformation matrix
      */
-    inline void transform_plane_as_vector_array(IK::Vector_3 *plane, const CGAL::Aff_transformation_3<IK> &xform)
-    {
-        IK::Point_3 p(plane[0].x(), plane[0].y(), plane[0].z());
-        p = xform.transform(p);
-
-        plane[0] = IK::Vector_3(p.x(), p.y(), p.z());
-
-        for (int i = 1; i < 4; i++)
-            plane[i] = xform.transform(plane[i]);
-    }
+    void transform_plane_as_vector_array(IK::Vector_3 *plane, const CGAL::Aff_transformation_3<IK> &xform);
 
     /**
      * assign c style array for boxes
@@ -215,38 +179,16 @@ namespace cgal_box_util
      * @param source 5 vectors that represents a box: origin, x, y, z, size (x,y,z)
      * @param target 5 vectors that represents a box: origin, x, y, z, size (x,y,z)
      */
-    inline void assign(const IK::Vector_3 *source, IK::Vector_3 *target, const int &n)
-    {
-        for (int i = 0; i < n; i++)
-            target[i] = source[i];
-    }
+    void assign(const IK::Vector_3 *source, IK::Vector_3 *target, const int &n);
 
     /**
      * check if two boxes are colliding
      *
-     * @param box1 5 vectors that represents a box: origin, x, y, z, size (x,y,z)
-     * @param box2 5 vectors that represents a box: origin, x, y, z, size (x,y,z)
+     * @param box1 5 vectors that represents a box: origin, x, y, z, half size (x,y,z)
+     * @param box2 5 vectors that represents a box: origin, x, y, z, half size (x,y,z)
      * @return bool value, if boxes are colliding
      */
-    inline bool get_collision(const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5])
-    {
-        IK::Vector_3 relative_position = box2[0] - box1[0];
-
-        return !(internal::get_separating_plane(relative_position, box1[1], box1, box2) ||
-                 internal::get_separating_plane(relative_position, box1[2], box1, box2) ||
-                 internal::get_separating_plane(relative_position, box1[3], box1, box2) ||
-                 internal::get_separating_plane(relative_position, box2[1], box1, box2) ||
-                 internal::get_separating_plane(relative_position, box2[2], box1, box2) ||
-                 internal::get_separating_plane(relative_position, box2[3], box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[1], box2[1]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[1], box2[2]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[1], box2[3]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[2], box2[1]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[2], box2[2]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[2], box2[3]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[3], box2[1]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[3], box2[2]), box1, box2) ||
-                 internal::get_separating_plane(relative_position, CGAL::cross_product(box1[3], box2[3]), box1, box2));
-    }
-
+    bool get_collision(const IK::Vector_3 (&box1)[5], const IK::Vector_3 (&box2)[5]);
 }
+
+#endif
