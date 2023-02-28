cdbe9579d94aceae749c6e4bfd9c57ed37bb735c
Petras Vestartas <petrasvestartas@gmail.com>
2023-02-28 17:23:53 +0100

    ADD python11 conversion to compas_wood read_xml

--- src/compas_wood/joinery.py
+++ src/compas_wood/joinery.py
@@ -1,4 +1,8 @@
 import wood_pybind11
+from wood_pybind11 import WoodVectorDouble
+from wood_pybind11 import WoodNestedVectorDouble
+from compas.geometry import Point
+from compas.geometry import Polyline
 
 
 def test():
@@ -16,19 +20,57 @@ def test():
     wood_pybind11.test()
 
 
-def read_xml():
-    """Test if Wood library is loaded
-
+def read_xml(
+    foldername, filename_of_dataset="type_plates_name_cross_vda_hexshell_reciprocal"
+):
+    """read xml data_sets files from foldername and filename_of_dataset
     Parameters
     ----------
-
+    foldername : str
+        The name of the folder where all the .xml data-sets are located e.g. "C://IBOIS57//_Code//Software//Python//compas_wood//frontend//src//wood//dataset//".
+    filename_of_dataset : str, optional
+        The name of the file e.g. "type_plates_name_cross_vda_hexshell_reciprocal" .
     Returns
     -------
-    Prints
-        Hello from CPP Wood
+    list[Polyline]
+        A list of top and bottom polylines
     """
 
-    wood_pybind11.test()
+    ###################################################################################################
+    # run the WOOD CPP code
+    ###################################################################################################
+    polylines_coordinates = WoodNestedVectorDouble(
+        []
+    )  # fills with double values e.g. WoodVectorDouble([0.5, 1.4, 2.3])
+
+    wood_pybind11.get_data_set(
+        "C://IBOIS57//_Code//Software//Python//compas_wood//frontend//src//wood//dataset//",
+        "type_plates_name_cross_vda_hexshell_reciprocal",
+        polylines_coordinates,
+    )
+
+    ###################################################################################################
+    # convert nested vector of doubles to a list of polylines
+    ###################################################################################################
+
+    output_polylines = []
+
+    for i in range(len(polylines_coordinates)):
+        polyline = Polyline([])
+        for j in range(0, len(polylines_coordinates[i], 3)):
+            polyline.append(
+                [
+                    polylines_coordinates[i][j + 0],
+                    polylines_coordinates[i][j + 1],
+                    polylines_coordinates[i][j + 2],
+                ]
+            )
+        output_polylines.append(polyline)
+
+    ###################################################################################################
+    # output
+    ###################################################################################################
+    return output_polylines
 
 
 def joints():
--- tests/compas_wood_2_read_xml.py
+++ tests/compas_wood_2_read_xml.py
@@ -1,4 +1,20 @@
 ####################################################################################################################################
+# Link the python module compiled in the release folder
+####################################################################################################################################
+import os.path as path
+import sys
+
+module_folder = (
+    path.abspath(path.join(__file__, "../../../..")) + "\\build_win\\Release"
+)
+print(module_folder)
+sys.path.append(module_folder)
+sys.path.append(
+    "C://IBOIS57//_Code//Software//Python//compas_wood//frontend//build_win//Release//"
+)
+
+
+####################################################################################################################################
 # Import the compas_wood module
 ####################################################################################################################################
 import wood_pybind11
@@ -11,7 +27,11 @@ polyline_1 = WoodVectorDouble([0.5, 1.4, 2.3])
 print(polyline_0)
 polylines_coordinates = WoodNestedVectorDouble([polyline_0, polyline_1])
 
-get_data_set("type_plates_name_cross_vda_hexshell_reciprocal", polylines_coordinates)
+get_data_set(
+    "C://IBOIS57//_Code//Software//Python//compas_wood//frontend//src//wood//dataset//",
+    "type_plates_name_cross_vda_hexshell_reciprocal",
+    polylines_coordinates,
+)
 
 for i in range(len(polylines_coordinates)):
     print(polylines_coordinates[i])
@@ -20,3 +40,4 @@ for i in range(len(polylines_coordinates)):
 # print(polylines_coordinates[0])
 # print(polylines_coordinates[1])
 # print(polylines_coordinates[2])
+
