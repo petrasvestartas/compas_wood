28a923660ecf2d6d1090fc4e96b525e7ec7ab6b7
Petras Vestartas <petrasvestartas@gmail.com>
2023-03-07 09:58:27 +0100

    MISC COMPAS docs empty

--- docs/conf.py
+++ docs/conf.py
@@ -155,7 +155,7 @@ def linkcode_resolve(domain, info):
     else:
         return None
 
-    return f"https://github.com/ibois-epfl/compas_wood/blob/master/src/{filename}.py#L{lineno}"
+    return f"https://github.com/petrasvestartas/compas_wood/blob/master/src/{filename}.py#L{lineno}"
 
 
 # extlinks
@@ -172,9 +172,9 @@ html_theme_options = {
     "package_title": project,
     "package_version": release,
     "package_author": "Petras Vestartas",
-    "package_docs": "https://ibois-epfl.github.io/compas_wood/",
-    "package_repo": "https://github.com/ibois-epfl/compas_wood",
-    "package_old_versions_txt": "https://ibois_epfl.github.io/compas-wood/doc_versions.txt",
+    "package_docs": "https://petrasvestartas.github.io/compas_wood/",
+    "package_repo": "https://github.com/petrasvestartas/compas_wood",
+    "package_old_versions_txt": "https://petrasvestartas.github.io/compas-wood/doc_versions.txt",
 }
 
 html_context = {}
