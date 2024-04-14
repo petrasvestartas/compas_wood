61e9e2063b261e93b8e8c654a3363e970ad283ca
Petras Vestartas <petrasvestartas@gmail.com>
2024-04-14 21:04:55 +0200

    LINT

--- src/compas_wood/binding/__init__.py
+++ src/compas_wood/binding/__init__.py
@@ -1,4 +1,4 @@
-from .binding_connection_zones import get_connection_zones
+from .binding_get_connection_zones import get_connection_zones
 from .binding_rtree import rtree
 
 __all__ = ["get_connection_zones", "rtree"]
