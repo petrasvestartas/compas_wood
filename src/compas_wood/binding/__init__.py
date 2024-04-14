98161a18c0dc1030ee0942e90b5b0a8d56a84180
Petras Vestartas <petrasvestartas@gmail.com>
2024-04-14 21:35:48 +0200

    ADD binding_test

--- src/compas_wood/binding/__init__.py
+++ src/compas_wood/binding/__init__.py
@@ -1,4 +1,5 @@
 from .binding_get_connection_zones import get_connection_zones
 from .binding_rtree import rtree
+from .binding_test import test
 
-__all__ = ["get_connection_zones", "rtree"]
+__all__ = ["get_connection_zones", "rtree", "test"]
