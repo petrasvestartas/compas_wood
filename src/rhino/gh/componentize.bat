55ef4b5765daaff0a88fa840dde667e2d9e5a5b6
vboxuser <petrasvestartas@gmail.com>
2024-04-25 01:13:25 +0200

    MISC

--- src/rhino/gh/componentize.bat
+++ src/rhino/gh/componentize.bat
@@ -4,5 +4,5 @@ REM Install pythonnet
 pip install pythonnet
 
 REM Run the desired command
-@REM python src\rhino\gh\componentize_cpy.py src\rhino\gh\cpy src\rhino\gh\build
+python src\rhino\gh\componentize_cpy.py src\rhino\gh\cpy src\rhino\gh\build
 python src\rhino\gh\componentize_cpy.py src\rhino\gh\cpy %APPDATA%\Grasshopper\UserObjects
